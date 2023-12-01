0fa6728456f47ada72bacdcb4dd995aee8655af6
karina4343 <kari_4343@hotmail.com>
2023-12-01 17:19:34 -0300

    nuevo 1/12

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Contacto from "./components/Contacto";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
@@ -18,6 +19,7 @@ function App (){
           <Route path="/productos" element={<ItemListContainer/>} />
           <Route path="/productos/:categoria" element={<ItemListContainer/>} />
           <Route path ="/nosotros" element = {<Nosotros/>} />
+          <Route path="/contacto" element= {<Contacto/>} />
         </Routes>
 
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,15 +6,14 @@ const NavBar = () => {
     <nav className='navbar'>
         <Link to="/" className='logo'> <h1>Libreria Nico</h1></Link>
         <ul className='menu'>
-            <li><Link className='menu-link' to="/">Inicio</Link></li>
+            
             <li><Link className='menu-link' to="/productos">Productos</Link></li>
             <li><Link className='menu-link' to="/productos/Cuadernos">Cuadernos</Link></li>
-            <li><Link className='menu-link' to="/productos/Adhesivos">Adhesivos</Link></li>
-            <li><Link className='menu-link' to="/productos/Cintas">Cintas</Link></li>
-            <li><Link className='menu-link' to="/productos/Escritura">Escritura</Link></li>
-            <li><Link className='menu-link' to="/productos/Geometria">Geometria</Link></li>
+            <li><Link className='menu-link' to="/productos/Adhesivos y Cintas">Adhesivos y Cintas</Link></li>
+            <li><Link className='menu-link' to="/productos/Escritura y Geometria">Escritura y Geometria</Link></li>
             <li><Link className='menu-link' to="/productos/Marcadores y Resaltadores">Marcadores y resaltadores</Link></li>
-            <li><Link className='menu-link' to="/productos/Accesorio">Accesorio</Link></li>
+            <li><Link className='menu-link' to="/contacto">Contacto</Link></li>
+            
             
         </ul>
     </nav>
